bc8bb61dd8617cbcf557b228abbb1f58722e1422
Zhao Li <zhao.li@alumni.usc.edu>
2019-05-08 10:29:16 -1000

    Allow script to support positional and optional arguments

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,22 +3,36 @@
 # This script bootstraps the application
 
 set -e
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ### define functions
-SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck source=scripts/bootstrap/functions.sh
 . "$SRC_DIR"/scripts/bootstrap/functions.sh
 
+# set_defaults
+#
+# sets up the defaults to be used later
+#
+# example usage: set_defaults
 set_defaults() {
-  ENV=development
+  environment=development
 }
 
+# gather_options
+#
+# gathers the options provided by the user
+# based off of http://linuxcommand.org/lc3_wss0120.php
+#
+# example usage: gather_options "$@"
 gather_options() {
-  while getopts "e:" OPT; do
-    case $OPT in
-      e)
-        ENV=$OPTARG
+  while [ "$1" != "" ]; do
+    case $1 in
+      -e)
+        environment=$2
+        shift
       ;;
     esac
+    shift
   done
 }
 ### define functions:end
@@ -26,11 +40,11 @@ gather_options() {
 set_defaults
 gather_options "$@"
 
-case $ENV in
+case $environment in
   'development')
     echo "Clearing out old containers"
-    teardown_containers "$ENV"
-    echo "Setting up $ENV containers"
-    setup_containers "$ENV"
+    teardown_containers "$environment"
+    echo "Setting up $environment containers"
+    setup_containers "$environment"
   ;;
 esac
