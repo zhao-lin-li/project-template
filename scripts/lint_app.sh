09023e99fce150d1b825801534a7b3f1990e37a2
Zhao Li <zhao.li@alumni.usc.edu>
2021-11-25 09:16:42 -1000

    fix linter findings

--- scripts/lint_app.sh
+++ scripts/lint_app.sh
@@ -2,8 +2,15 @@
 
 # This script lints the application
 
-shellcheck -- *.sh
+findings_found=0
+_update_findings_count() {
+  findings_found=$((findings_found + 1))
+}
+
+shellcheck --external -- *.sh || _update_findings_count
 for file in scripts/**/*.sh; do
-  shellcheck "$file"
+  shellcheck --external "$file" || _update_findings_count
 done
+
+exit $findings_found
 
