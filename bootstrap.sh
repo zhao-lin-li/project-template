d2fd16da925bf1e987b64f40fcac03aba0817f2a
Zhao Li <zhao.li@alumni.usc.edu>
2022-10-11 16:15:34 -1000

    abstract functions into separate file

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,37 +5,8 @@
 set -e
 SRC_DIR=$(cd "$(dirname "$0")" || exit; pwd -P)
 
-### define functions
 # shellcheck source=scripts/bootstrap/functions.sh
 . "$SRC_DIR"/scripts/bootstrap/functions.sh
-
-# set_defaults
-#
-# sets up the defaults to be used later
-#
-# example usage: set_defaults
-set_defaults() {
-  environment=development
-}
-
-# gather_options
-#
-# gathers the options provided by the user
-# based off of http://linuxcommand.org/lc3_wss0120.php
-#
-# example usage: gather_options "$@"
-gather_options() {
-  while [ "$1" != "" ]; do
-    case $1 in
-      -e)
-        environment=$2
-        shift
-      ;;
-    esac
-    shift
-  done
-}
-### define functions:end
 
 set_defaults
 gather_options "$@"
@@ -46,6 +17,9 @@ case $environment in
     teardown_containers "$environment"
     echo "Setting up $environment containers"
     setup_containers "$environment"
+  ;;
+  'production')
+    echo "Do production stuff" # placeholder
   ;;
 esac
 
--- scripts/bootstrap/functions.sh
+++ scripts/bootstrap/functions.sh
@@ -5,6 +5,33 @@
 # https://github.com/thoughtbot/laptop/issues/481
 # https://stackoverflow.com/questions/18597697/posix-compliant-way-to-scope-variables-to-a-function-in-a-shell-script#answer-18600920
 
+# set_defaults
+#
+# sets up the defaults to be used later
+#
+# example usage: set_defaults
+set_defaults() {
+  environment=development
+}
+
+# gather_options
+#
+# gathers the options provided by the user
+# based off of http://linuxcommand.org/lc3_wss0120.php
+#
+# example usage: gather_options "$@"
+gather_options() {
+  while [ "$1" != "" ]; do
+    case $1 in
+      -e)
+        environment=$2
+        shift
+      ;;
+    esac
+    shift
+  done
+}
+
 # teardown_containers
 #
 # clear out containers of any remnants of the application
